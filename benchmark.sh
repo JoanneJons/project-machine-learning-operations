09a05c29698099136ad5d7b3be0ac520ebb7f9ec
Joanne Jons <joannejons@gmail.com>
2021-01-13 14:46:44 +0530

    Experiment done

--- benchmark.sh
+++ benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer OLgGuRIHIhx9JeHlaZMp6jW3GeD4wb9a' http://3cb5e951-3c1d-4bf0-8a9d-c3256dd4d1c4.southcentralus.azurecontainer.io/score
--- endpoint.py
+++ endpoint.py
@@ -3,9 +3,9 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://3cb5e951-3c1d-4bf0-8a9d-c3256dd4d1c4.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'OLgGuRIHIhx9JeHlaZMp6jW3GeD4wb9a'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
